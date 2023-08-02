2af0b6f55dab43aa3fb5f063981e1df23867d5cf
han-mg <alrb1277@gmail.com>
2023-08-02 23:35:33 +0900

    ddd

--- see_near/models.py
+++ see_near/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class blog_user(models.Model):
-    id=models.TextField(max_length=100)
+    iddddddddddd=models.TextField(max_length=100)
     password=models.TextField(max_length=200)
     nickname=models.TextField(max_length=200)
     user_name=models.TextField(max_length=200)
