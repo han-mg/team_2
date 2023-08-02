20b6905aa5bd9935ad8bc9a020e7da7b52266789
han-mg <alrb1277@gmail.com>
2023-08-02 22:50:21 +0900

    연습

--- see_near/models.py
+++ see_near/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class blog_user(models.Model):
+    id=models.TextField(max_length=100)
+    password=models.TextField(max_length=200)
+    nickname=models.TextField(max_length=200)
+    user_name=models.TextField(max_length=200)
+    email=models.EmailField(max_length=254)
+    
