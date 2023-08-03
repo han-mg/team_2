7e0d11f8589cda4c8a57b27e179eb3925faeeaaa
han-mg <alrb1277@gmail.com>
2023-08-03 14:37:33 +0900

    seenear_user models.py수정

--- see_near/models.py
+++ see_near/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 
 # Create your models here.
-class blog_user(models.Model):
-    id=models.TextField(max_length=100)
+class seenear_user(models.Model):
+    user_id=models.TextField(max_length=100)
     password=models.TextField(max_length=200)
     nickname=models.TextField(max_length=200)
     user_name=models.TextField(max_length=200)
     email=models.EmailField(max_length=254)
-    
+    address=models.TextField(max_length=200)
+    user_number=models.TextField(max_length=200)
+    reg_date=models.DateTimeField(auto_now_add=True)
